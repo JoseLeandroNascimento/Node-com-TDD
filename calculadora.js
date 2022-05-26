9b6b5bda95f61e6fda42048f6868ae18ee93f9b1
j.leandro1998silva@gmail.com <j.leandro1998silva@gmail.com>
2022-05-25 22:48:13 -0500

    Aplicaçao cnode com TDD

---
+++ calculadora.js
@@ -0,0 +1,21 @@
+
+module.exports ={
+
+    soma:function(n1, n2){
+
+        return n1 + n2;
+    },
+    subtracao: function(n1, n2){
+
+        return n1 -n2
+    },
+    multiplicacao: function(n1, n2){
+
+        return n1 * n2;
+    },
+    divisao: function(n1,n2){
+
+        return n1 /n2;
+
+    }
+}
---
+++ test/calculadora.test.js
@@ -0,0 +1,21 @@
+const calculadora = require('../calculadora')
+
+test("A soma de 5 + 5 deve retornar 10",()=>{
+
+    expect(calculadora.soma(5,5)).toEqual(10)
+})
+
+test("A subtracao de 4-2 deve ser 2",()=>{
+
+    expect(calculadora.subtracao(4,2)).toEqual(2)
+})
+
+test("A multiplicacao de 10x2 deve ser 20",()=>{
+
+    expect(calculadora.multiplicacao(10,2)).toEqual(20)
+})
+
+test("A divisao de 4/2 deve ser 2",()=>{
+
+    expect(calculadora.divisao(4,2)).toEqual(2)
+})
